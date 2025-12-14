22309c5608e42af7563bc5370f6ffb48c884ada3
Vithusa Muttaiya <6232542@johnabbottcollege.net>
2025-12-14 13:47:58 -0500

    added the images to assets.json and imagename

--- src/enums/ImageName.js
+++ src/enums/ImageName.js
@@ -6,7 +6,10 @@ export const ImageName = {
 	Badniks: "badniks",
 	TitleScreen: "titlescreen",
 	TitleScreenTopBG: 'titlescreen-topbg',
-	TitleScreenBottomBG: 'titlescreen-bottombg'
+	TitleScreenBottomBG: 'titlescreen-bottombg',
+	LevelTopBG: 'level-topbg',
+	LevelMiddleBG: 'level-middlebg',
+	LevelBottomBG: 'level-bottombg'
 };
 
 export default ImageName;
